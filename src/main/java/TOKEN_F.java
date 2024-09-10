f11d02e7812bf080a74e8dcc4f9dcbd87324b74d
Thando Dlamini <u22906160@tuks.co.za>
2024-09-10 08:21:16 +0200

    src: Token classes implemented

--- src/main/java/TOKEN_F.java
+++ src/main/java/TOKEN_F.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TOKEN_F {
-    TOKEN_F(){
-        this.regex_string = "";
-    }
-    private String regex_string;
+    private final String regex_string;
     private String [] function_tokens;
+
+    public TOKEN_F() {
+        this.regex_string = "F_[a-z]([a-z]|[0-9])*";
+        this.function_tokens = new String[0];
+    }
 
     public String[] getFunction_tokens() {
         return function_tokens;
@@ -11,5 +18,20 @@ public class TOKEN_F {
 
     public void setFunction_tokens(String[] function_tokens) {
         this.function_tokens = function_tokens;
+    }
+
+    public String [] splitFunctions(String string)
+    {
+        Pattern pattern = Pattern.compile(this.regex_string);
+
+        Matcher matcher = pattern.matcher(string);
+
+        List<String> matchedFunctions = new ArrayList<>();
+        while (matcher.find()) {
+            matchedFunctions.add(matcher.group());
+        }
+
+        this.function_tokens = matchedFunctions.toArray(new String[0]);
+        return this.function_tokens;
     }
 }
--- src/main/java/TOKEN_T.java
+++ src/main/java/TOKEN_T.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TOKEN_T {
-    TOKEN_T(){
-        this.regex_string = "";
-    }
-    private String regex_string;
+    private final String regex_string;
     private String [] text_tokens;
+
+    public TOKEN_T() {
+        regex_string = "\"[A-Z][a-z]{1,7}\"";
+        text_tokens = new String[0];
+    }
 
     public String[] getText_tokens() {
         return text_tokens;
@@ -11,5 +18,20 @@ public class TOKEN_T {
 
     public void setText_tokens(String[] text_tokens) {
         this.text_tokens = text_tokens;
+    }
+
+    public String [] splitText(String string)
+    {
+        Pattern pattern = Pattern.compile(regex_string);
+
+        Matcher matcher = pattern.matcher(string);
+
+        List<String> matchedText = new ArrayList<>();
+        while (matcher.find()) {
+            matchedText.add(matcher.group());
+        }
+
+        text_tokens = matchedText.toArray(new String[0]);
+        return text_tokens;
     }
 }
