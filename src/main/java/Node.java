1bdf2505f19f9326d576342c2cb79853f89e9fad
bob <siphelelebobdlamini@gmail.com>
2024-10-22 16:10:08 +0200

    update: added a parent field to the node so that its easier to travers the tree in the typechecker of the program and added the TypeChecker.java file for modularity and separation of concerns in the program

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -5,11 +5,13 @@ public class Node {
     public int unid;
     public String symbol;
     public List<Node> children;
+    private Node parent;
 
     public Node(int unid, String symbol) {
         this.unid = unid;
         this.symbol = symbol;
         this.children = new ArrayList<>();
+        this.parent = null;
     }
 
     public int getUnid() {
@@ -26,9 +28,18 @@ public class Node {
 
     public void addChild(Node child) {
         children.add(child);
+        child.parent = this;
     }
 
     public String getName() {
         return symbol;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
     }
 }
